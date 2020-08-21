629e99f03ae770980ce090c76fc918d8f20c0100
関口 司 <t14159265358979323846@gmail.com>
2020-08-21 23:35:24 +0900

    init php-cs-fixer

---
+++ app/Console/Commands/PhpCsFixer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PhpCsFixer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fix';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'php-cs-fixer\'s command';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->info(shell_exec("./vendor/bin/php-cs-fixer fix ./app/ && ./vendor/bin/php-cs-fixer fix ./tests/"));
+    }
+}
