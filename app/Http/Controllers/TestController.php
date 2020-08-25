fae0617d96eacdfbb0aa0c8cc2077c36df86ba3b
関口 司 <t14159265358979323846@gmail.com>
2020-08-25 19:27:15 +0900

    test0825_001

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    // public function func0()
+    // {
+    //     \Log::debug(1);
+    //     \Log::debug(1);
+    //     \Log::debug(1);
+    //     \Log::debug(1);
+    //     \Log::debug(1);
+    //     \Log::debug(1);
+    //     \Log::debug(1);
+    //     \Log::debug(1);
+    //     return $this->subFunc1();
+    // }
+
+    /**
+     * Undocumented function
+     *
+     * @return int
+     */
+    public function func1()
+    {
+        return $this->subFunc1();
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return string
+     */
+    private function subFunc1()
+    {
+        return '1';
+    }
+
+//     public function func2()
+//     {
+//         return $this->subFunc2();
+//     }
+
+//     public function func3()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func4()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func5()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func6()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func7()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func8()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func9()
+//     {
+//         return $this->subFunc1();
+//     }
+//     public function func10()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func11()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func12()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func13()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func14()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func15()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func16()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func17()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func18()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func19()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func20()
+//     {
+//         return $this->subFunc1();
+//     }
+
+//     public function func21()
+//     {
+//         $a = 1;
+//         if ($a === 1) {
+//             if ($a === 2) {
+//                 if ($a === 3) {
+//                     if ($a === 4) {
+//                         return true;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+}
